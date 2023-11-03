d6a7c1437b6771272f4aa1267a6b0b73a61e0940
joshuajtward <joshuajtward+work@gmail.com>
2023-11-03 11:17:41 +0000

    update packages

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,10 +2,8 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   defaultCommandTimeout: 200,
-
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    fixturesFolder: false,
+    setupNodeEvents: (on, config) => config,
   },
 });
